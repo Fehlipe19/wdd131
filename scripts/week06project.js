4d5b7755eae6051c2d587633f8aa0f2bcc6a35f2
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-08-05 21:16:02 -0400

    Style home page

--- scripts/week06project.js
+++ scripts/week06project.js
@@ -5,3 +5,20 @@ menuButton.addEventListener("click", () => {
   navList.classList.toggle("open");
   menuButton.classList.toggle("active");
 });
+
+const diceButton = document.getElementById("dice-click");
+const diceText = document.getElementById("result");
+const resultText = document.getElementById("extreme");
+
+diceButton.addEventListener("click", () => {
+  var diceRoll = Math.floor(Math.random() * 20 + 1);
+  diceText.textContent = diceRoll;
+
+  // if (diceRoll == 20) {
+  //   resultText.textContent = "Perfect Success!";
+  // } else if (diceRoll == 1) {
+  //   resultText.textContent = "Critical Failure!";
+  // } else {
+  //   resultText.textContent = ".";
+  // }
+});
