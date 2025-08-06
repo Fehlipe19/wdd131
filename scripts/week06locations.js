d233fab24188ce39997c2db8ce795c2e9e5a9c66
Felipe Menezes <168611944+Fehlipe19@users.noreply.github.com>
2025-08-06 01:18:55 -0400

    Add local storage to form

---
+++ scripts/week06locations.js
@@ -0,0 +1,14 @@
+const searchSubmission = "form_submitted";
+const currentURL = window.location.href;
+const message = document.querySelector("h1");
+console.log(currentURL.includes(searchSubmission));
+
+document.addEventListener("DOMContentLoaded", () => {
+  if (currentURL.includes(searchSubmission)) {
+    let counter = parseInt(localStorage.getItem("loations-submitted") || "0", 10);
+    console.log(counter);
+    counter++;
+    let counter_string = counter.toString();
+    localStorage.setItem("locations-submitted", counter_string);
+  }
+});
